c4b40036da77a0a890deb066337472c899a5715c
Sumit Khinchi <creativekhinchi@gmail.com>
2019-08-22 18:36:38 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,12 @@
+insert into Students values 
+(100,'Sumit','sk@gmail.com');
+
+insert into Students values
+ (101,'Ankit','ankit@gmail.com');
+
+insert into Students values
+ (102,'Anwar','anwar@gmail.com');
+ 
+ insert into Students values
+ (103,'Aryan','aryan@gmail.com');
+ 
