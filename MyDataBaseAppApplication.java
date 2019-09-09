690a56e2e877dcbc17fe52b7a02ab65ba1be86a1
Sumit Khinchi <creativekhinchi@gmail.com>
2019-09-09 19:22:03 +0530

    Create MyDataBaseAppApplication.java

---
+++ MyDataBaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDataBaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDataBaseAppApplication.class, args);
+	}
+
+}
